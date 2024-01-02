03e8dc08c2051982f7aa4abe1c7b0c1d501ca735
Huero Bruce Rodas <rodas.huerobruce@gmail.com>
2024-01-01 20:55:39 -0800

    Add powershell local set-up.

---
+++ local-setup.ps1
@@ -0,0 +1,19 @@
+if (Get-Command git)
+{
+    write-host "git already installed"
+}
+else
+{
+    write-host "installing git"
+    winget install -e --id Git.Git --accept-source-agreements --accept-package-agreements
+}
+
+if (Get-Command code)
+{
+    write-host "code already installed"
+}
+else
+{
+    write-host "installing code"
+    winget install -e --id Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
+}
